80fd95a4e1ea3d7e27202c8847cee35cd6835dd7
Faruk Spahic <farukspahictz@gmail.com>
2023-05-12 16:02:58 +0200

    Added a key to each cell.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Board = () => {
   const createCell = (row: number, col: number) => {
     return (
-      <div className="w-[10rem] h-[10rem] border border-gray-200">{`${row}, ${col}`}</div>
+      <div
+        key={`${row}-${col}`}
+        className="w-[10rem] h-[10rem] border border-gray-200"
+      >{`${row}, ${col}`}</div>
     );
   };
 
