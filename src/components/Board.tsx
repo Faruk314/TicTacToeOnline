8d936e3141253df5342d6b5a5539fbda91f43374
Faruk Spahic <farukspahictz@gmail.com>
2023-05-12 15:50:24 +0200

    Added a function that creates a single row.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,11 +3,21 @@ import React from "react";
 const Board = () => {
   const createCell = (row: number, col: number) => {
     return (
-      <div className="w-20 h-20 border border-gray-200">{`${row}, ${col}`}</div>
+      <div className="w-[10rem] h-[10rem] border border-gray-200">{`${row}, ${col}`}</div>
     );
   };
 
-  return <div>{createCell(1, 2)}</div>;
+  const createRow = (row: number) => {
+    const cells = [];
+
+    for (let col = 0; col < 3; col++) {
+      cells.push(createCell(row, col));
+    }
+
+    return <div className="flex">{cells}</div>;
+  };
+
+  return <div>{createRow(1)}</div>;
 };
 
 export default Board;
