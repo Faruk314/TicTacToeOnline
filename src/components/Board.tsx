a8c8799fef888b2a14f7008db849272e9db2db22
Faruk Spahic <farukspahictz@gmail.com>
2023-05-12 18:21:22 +0200

    Changed my functionality for board creation and created a handleClick function.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,38 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Board = () => {
-  const createCell = (row: number, col: number) => {
-    return (
-      <div
-        key={`${row}-${col}`}
-        className="w-[10rem] h-[10rem] border border-gray-200"
-      >{`${row}, ${col}`}</div>
-    );
+  const [board, setBoard] = useState<string[][]>([]);
+  const [playerTurn, setPlayerTurn] = useState("X");
+
+  console.log(board);
+
+  const handleClick = (row: number, col: number) => {
+    if (board[row][col] !== "") return;
+
+    let newBoard = [...board];
+
+    newBoard[row][col] = playerTurn;
+
+    setBoard(newBoard);
+
+    setPlayerTurn(playerTurn === "X" ? "O" : "X");
   };
 
-  const createRow = (row: number) => {
-    const cells = [];
+  useEffect(() => {
+    const createBoard = () => {
+      let board = [];
 
-    for (let col = 0; col < 3; col++) {
-      cells.push(createCell(row, col));
-    }
+      for (let row = 0; row < 3; row++) {
+        let row = [];
+        for (let col = 0; col < 3; col++) {
+          row.push("");
+        }
+        board.push(row);
+      }
 
-    return <div className="flex">{cells}</div>;
-  };
+      setBoard(board);
+    };
 
-  const createBoard = () => {
-    const board = [];
-
-    for (let row = 0; row < 3; row++) {
-      board.push(createRow(row));
-    }
-
-    return <div>{board}</div>;
-  };
+    createBoard();
+  }, []);
 
   return (
     <div className="flex items-center justify-center h-[100vh]">
-      {createBoard()}
+      <div>
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex">
+            {row.map((col, colIndex) => (
+              <div
+                onClick={() => handleClick(rowIndex, colIndex)}
+                key={colIndex}
+                className="w-[10rem] h-[10rem] border border-gray-200 flex items-center justify-center"
+              >
+                <span className="text-[5rem]">{col}</span>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
