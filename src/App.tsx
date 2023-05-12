3bfde0d48712a6c25225f7b578125fa0c28410b8
Faruk Spahic <farukspahictz@gmail.com>
2023-05-12 15:40:26 +0200

    Created a Board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Board from "./components/Board";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Board />
+    </div>
+  );
 }
 
 export default App;
