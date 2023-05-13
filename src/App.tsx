e341bd6235b24480d46482d4b5d5325d07bc3e9f
Faruk Spahic <farukspahictz@gmail.com>
2023-05-13 15:43:40 +0200

    Added some of the routes to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Board from "./components/Board";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="">
+    <BrowserRouter>
       <Navbar />
-      <Board />
-    </div>
+      <Routes>
+        <Route element={<Login />} />
+        <Route element={<Board />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
