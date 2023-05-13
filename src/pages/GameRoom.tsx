e82b04fa26ddbc954e790c7670fac8716222a3a6
Faruk Spahic <farukspahictz@gmail.com>
2023-05-13 16:13:45 +0200

    Created a GameRoom page which contains Navbar and Board component.

---
+++ src/pages/GameRoom.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "../components/Board";
+import Navbar from "../components/Navbar";
+
+const GameRoom = () => {
+  return (
+    <section>
+      <Navbar />
+      <Board />
+    </section>
+  );
+};
+
+export default GameRoom;
