9f46f142fdc26009fff4e4ef01552f342158697f
Faruk Spahic <farukspahictz@gmail.com>
2023-05-13 16:16:43 +0200

    Removed Navbar import from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import MainMenu from "./pages/MainMenu";
