14f6a5f4807d9484a8d4df340f9fea5504cecd66
Faruk Spahic <farukspahictz@gmail.com>
2023-05-13 20:46:23 +0200

    Deleted title from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between shadow-xl py-2 px-2">
-      <h1 className="text-2xl font-bold">TicTacToe</h1>
       <GiHamburgerMenu size={25} />
     </div>
   );
