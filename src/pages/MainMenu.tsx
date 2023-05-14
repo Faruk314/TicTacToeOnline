b46ac16d0c7b7f33848c90aaf995171e67fa67ab
Faruk Spahic <farukspahictz@gmail.com>
2023-05-14 13:46:07 +0200

    Added MuteButton in MainMenu

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { RiComputerLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import MuteButton from "../components/MuteButton";
+import { useAppDispatch } from "../redux/hooks";
+import { toggleMute } from "../redux/SoundSlice";
 
 const MainMenu = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <section className="flex flex-col items-center text-center justify-center h-[100vh]">
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+          dispatch(toggleMute());
+        }}
+        className="absolute top-2 right-2"
+      >
+        <MuteButton />
+      </div>
+
       <img src="/images/logo.jpg" alt="" className="w-[9rem] mb-5" />
 
       <div className="shadow-xl py-10 w-[18rem] rounded-md">
