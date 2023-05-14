0dbff962c3f5206292c39aec2550808d38124d23
Faruk Spahic <farukspahictz@gmail.com>
2023-05-14 15:34:52 +0200

    Added click sounds to items in settings menu

--- src/modals/Settings.tsx
+++ src/modals/Settings.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MuteButton from "../components/MuteButton";
 import { useAppDispatch } from "../redux/hooks";
-import { toggleMute } from "../redux/SoundSlice";
+import { playClickSound, toggleMute } from "../redux/SoundSlice";
 import { IoExitOutline } from "react-icons/io5";
 
 interface Props {
@@ -15,20 +15,31 @@ const Settings = ({ setOpen }: Props) => {
     <section className="fixed top-0 bottom-0 left-0 right-0 z-20 flex flex-col items-center justify-center text-center bg-[rgb(0,0,0,0.5)]">
       <div className="relative shadow-xl py-10 w-[18rem] rounded-md z-30 bg-white">
         <div
-          onClick={() => dispatch(toggleMute())}
+          onClick={() => {
+            dispatch(playClickSound("/sounds/click.wav"));
+            dispatch(toggleMute());
+          }}
           className="flex justify-center py-2 space-x-3 text-xl font-bold cursor-pointer hover:bg-gray-100"
         >
           <span className="">MUTE SOUND</span>
           <MuteButton />
         </div>
 
-        <div className="flex items-center justify-center py-2 space-x-3 text-xl font-bold cursor-pointer hover:bg-gray-100">
+        <div
+          onClick={() => {
+            dispatch(playClickSound("/sounds/click.wav"));
+          }}
+          className="flex items-center justify-center py-2 space-x-3 text-xl font-bold cursor-pointer hover:bg-gray-100"
+        >
           <span>LEAVE GAME</span>
           <IoExitOutline size={25} />
         </div>
 
         <button
-          onClick={() => setOpen(false)}
+          onClick={() => {
+            dispatch(playClickSound("/sounds/click.wav"));
+            setOpen(false);
+          }}
           className="absolute top-[-0.5rem] px-2 font-bold border-2 border-black rounded-full right-[-0.5rem] bg-white hover:bg-gray-200"
         >
           X
