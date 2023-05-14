d77d509c91dd829d124b3a0df726e1948df4a6b4
Faruk Spahic <farukspahictz@gmail.com>
2023-05-14 15:26:35 +0200

    With this change PopUp sound will play when we enter the settings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import { IoMdSettings } from "react-icons/io";
 import Settings from "../modals/Settings";
+import { useAppDispatch } from "../redux/hooks";
+import { playPopUpSound } from "../redux/SoundSlice";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="flex justify-end p-4">
-      <button onClick={() => setOpen(true)} className="">
+      <button
+        onClick={() => {
+          dispatch(playPopUpSound("/sounds/popUp.mp3"));
+          setOpen(true);
+        }}
+        className=""
+      >
         <IoMdSettings size={25} />
       </button>
 
