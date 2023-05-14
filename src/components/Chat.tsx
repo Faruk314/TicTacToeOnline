d8df533f3ba3a8f7d238d2ae639b67928705016c
Faruk Spahic <farukspahictz@gmail.com>
2023-05-14 12:02:37 +0200

    Changed chat style

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,13 +4,13 @@ import Message from "../cards/Message";
 const Chat = () => {
   return (
     <div className="flex flex-col fixed border-2 border-black bottom-0 right-0 h-[20rem] w-full md:w-[20rem]">
-      <div className="h-full overflow-y-auto bg-gray-200">
-        <Message />
+      <div className="h-full overflow-y-auto bg-gray-50">
+        <Message message={"hello"} />
       </div>
 
-      <div className="flex space-x-1 items-center h-[3rem] p-1 bg-gray-200">
+      <div className="flex space-x-1 items-center h-[3rem] p-1">
         <textarea
-          className="w-full h-full px-2 border border-black"
+          className="w-full h-full px-2 border-2 border-black outline-none"
           placeholder="Enter your message here"
           rows={2}
         />
