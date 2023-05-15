125076c420bba3dd5700d9105a71c3d52f00caa1
Faruk Spahic <farukspahictz@gmail.com>
2023-05-15 14:34:43 +0200

    Added getLoginStatus function in useEffect inside of App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GameRoom from "./pages/GameRoom";
 import Login from "./pages/Login";
 import MainMenu from "./pages/MainMenu";
 import Register from "./pages/Register";
 import axios from "axios";
+import { useAppDispatch } from "./redux/hooks";
+import { getLoginStatus } from "./redux/AuthSlice";
 
 axios.defaults.withCredentials = true;
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getLoginStatus());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Routes>
