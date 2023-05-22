f5a921d641a4f086f2d444b201cb32afbb4ff14d
Faruk Spahic <farukspahictz@gmail.com>
2023-05-22 15:20:54 +0200

    Modified findUser controller to not include user that is currently logged

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -4,13 +4,14 @@ import query from "../db";
 
 export const findUsers = asyncHandler(async (req: Request, res: Response) => {
   const { search } = req.query;
+  const loggedUser = req.user?.userId;
 
   const searchTerm = `%${search}%`;
 
   let q =
-    "SELECT `user_id` AS userId,`user_name` AS userName,`image` FROM users WHERE `user_name` LIKE ? OR `user_id` LIKE ?";
+    "SELECT `user_id` AS userId, `user_name` AS userName, `image` FROM users WHERE (`user_name` LIKE ? OR `user_id` LIKE ?) AND `user_id` <> ?";
 
-  let data = await query(q, [searchTerm, searchTerm]);
+  let data = await query(q, [searchTerm, searchTerm, loggedUser]);
 
   res.status(200).json(data);
 });
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,8 +1,9 @@
 import express, { Router, Request, Response } from "express";
 import { findUsers } from "../controllers/user";
+import { protect } from "../utils/protect";
 
 const router: Router = express.Router();
 
-router.get("/findUsers", findUsers);
+router.get("/findUsers", protect, findUsers);
 
 export default router;
